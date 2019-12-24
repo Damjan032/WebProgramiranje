554eb366058e8971f45a86cced43d0e756101f62
nklppc <neekolapapic@gmail.com>
2019-12-24 17:23:02 +0100

    Spojeno

--- src/models/Sistem.java
+++ src/models/Sistem.java
@@ -1,17 +1,19 @@
 package models;
 
+import models.komunikacija.Poruka;
 import models.moduli.OrganizacijeModul;
 import models.enums.Uloga;
 import models.komunikacija.LoginPoruka;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Sistem {
-    private HashMap<String, KorisnikNalog> korisnici = new HashMap<>();
-    private OrganizacijeModul orgController = OrganizacijeModul.getInstance();
     private HashMap<String, KorisnikNalog> korisniciNalozi = new HashMap<>();
+    private OrganizacijeModul orgController = OrganizacijeModul.getInstance();
+    private List<Korisnik> korisnici= new ArrayList<>();
 
     public OrganizacijeModul getOrgController() {
         return orgController;
@@ -48,14 +50,4 @@ public class Sistem {
                 return null;
         }
     }
-    public Poruka addOrg(String name, String opis, String imgPath){
-        System.out.println("NAAAAAAAAAME" + name);
-        for (Organizacija o: organizacije) {
-            if (o.getIme().equalsIgnoreCase(name))
-                return new Poruka("Organizacija je vec uneta", false);
-        };
-        organizacije.add(new Organizacija(name,opis,imgPath, new ArrayList<>(), new ArrayList<>()));
-        return new Poruka("Uspesno uneta organizacija", true);
-    }
-
 }
